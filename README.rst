4e6161cb743b03ce7ecd03370bbb00043c1236bb
Johannes Raggam <thetetet@gmail.com>
2018-05-11 11:42:13 +0200

    initial

---
+++ README.rst
@@ -0,0 +1,19 @@
+======================
+collective.rendertimer
+======================
+
+Track timings for code parts in Plone based on events.
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/collective/collective.rendertimer/issues
+- Source Code: https://github.com/collective/collective.rendertimer
+- Documentation: https://docs.plone.org/foo/bar
+
+
+License
+-------
+
+The project is licensed under the GPLv2.
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.rendertimer package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CHANGES.rst').read(),
+])
+
+
+setup(
+    name='collective.rendertimer',
+    version='1.0a1',
+    description="Track timings for code parts in Plone based on events.",
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='Python Plone',
+    author='Johannes Raggam',
+    author_email='thetetet@gmail.com',
+    url='https://pypi.python.org/pypi/collective.rendertimer',
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        'setuptools',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
